7170da2b8c723a930a8bd7981e8f15664636cceb
bhavaniravi <bhava0895@gmail.com>
2021-01-28 19:36:01 +0530

    add a tutorial branch

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import json
 
 load_dotenv(verbose=True)
 
-from service import BookService
+# Import BookService here
 
 
 app = Flask(__name__)
@@ -17,33 +17,8 @@ def add_headers(response):
     response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
     return response
 
-@app.route("/book", methods=["GET"])
-def list_books():
-    return jsonify(BookService().get_all())
+# Add the Flask routes here
 
-
-@app.route("/book", methods=["POST"])
-def create_book():
-    return jsonify(BookService().create(request.get_json()))
-
-
-@app.route("/book/<book_id>", methods=["PUT"])
-def update_book(book_id):
-    return jsonify(BookService().update(book_id, request.get_json()))
-
-
-@app.route("/book/<book_id>", methods=["DELETE"])
-def delete_book(book_id):
-    return jsonify(BookService().delete(book_id))
-
-@app.route("/book/<book_id>", methods=["GET"])
-def get_book(book_id):
-    return jsonify(BookService().get(book_id))
-
-
-@app.route("/book/<book_id>/note", methods=["POST"])
-def add_notes(book_id):
-    return jsonify(BookService().add_notes(book_id, request.get_json()))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
