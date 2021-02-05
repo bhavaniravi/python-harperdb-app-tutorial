3dfa507878fecc63f81f734d7f7e87b2064d1663
bhavaniravi <bhava0895@gmail.com>
2021-02-05 12:32:05 +0530

    remove unwanted code and updated the insomnia file

--- app/app.py
+++ app/app.py
@@ -1,21 +1,11 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-import json
-
 load_dotenv(verbose=True)
 
 # Import BookService here
 
 
 app = Flask(__name__)
-
-
-@app.after_request
-def add_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
-    return response
 
 # Add the Flask routes here
 
