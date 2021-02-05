7289baee6bd70122772597e58e8647e273f3aa19
bhavaniravi <bhava0895@gmail.com>
2021-02-05 12:31:18 +0530

    remove unwanted code

--- app/app.py
+++ app/app.py
@@ -9,14 +9,6 @@ from service import BookService
 
 app = Flask(__name__)
 
-
-@app.after_request
-def add_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
-    return response
-
 @app.route("/book", methods=["GET"])
 def list_books():
     return jsonify(BookService().get_all())
